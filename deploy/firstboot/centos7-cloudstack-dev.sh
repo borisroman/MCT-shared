a1b977e94d5a1c4b57a63f3b3b1db3e99e85f09c
Boris Schrijver <boris@pcextreme.nl>
2015-07-29 16:43:03 +0200

    Changed SystemVM template.

--- deploy/firstboot/centos7-cloudstack-dev.sh
+++ deploy/firstboot/centos7-cloudstack-dev.sh
@@ -59,8 +59,13 @@ mysql -u cloud -pcloud cloud --exec "INSERT INTO cloud.configuration (instance, 
 
 # Adding the right SystemVMs, for KVM
 # Adding the tiny linux VM templates for KVM
-mysql -u cloud -pcloud cloud --exec "UPDATE cloud.vm_template SET url='http://jenkins.buildacloud.org/view/4.5/job/build-systemvm-4.5/lastSuccessfulBuild/artifact/tools/appliance/dist/systemvm64template-4.5-kvm.qcow2.bz2' where id=1;"
+mysql -u cloud -pcloud cloud --exec "UPDATE cloud.vm_template SET url='http://cloudstack.apt-get.eu/systemvm/4.5/systemvm64template-4.5-kvm.qcow2.bz2' where id=1;"
 mysql -u cloud -pcloud cloud --exec "UPDATE cloud.vm_template SET url='http://dl.openvm.eu/cloudstack/macchinina/x86_64/macchinina-kvm.qcow2.bz2', guest_os_id=140, name='tiny linux kvm', display_text='tiny linux kvm', hvm=1 where id=2;"
+
+# Open ports for Cloudstack
+firewall-cmd --permanent --add-port=8080/tcp
+firewall-cmd --permanent --add-port=8081/tcp
+firewall-cmd --permanent --add-port=8787/tcp
 
 # Reboot
 reboot
--- helper_scripts/cloudstack/build_run.sh
+++ helper_scripts/cloudstack/build_run.sh
@@ -13,7 +13,7 @@ mysql -u cloud -pcloud cloud --exec "INSERT INTO cloud.configuration (instance, 
 
 # Adding the right SystemVMs, for KVM
 # Adding the tiny linux VM templates for KVM
-mysql -u cloud -pcloud cloud --exec "UPDATE cloud.vm_template SET url='http://jenkins.buildacloud.org/view/4.5/job/build-systemvm-4.5/lastSuccessfulBuild/artifact/tools/appliance/dist/systemvm64template-4.5-kvm.qcow2.bz2' where id=1;"
+mysql -u cloud -pcloud cloud --exec "UPDATE cloud.vm_template SET url='http://cloudstack.apt-get.eu/systemvm/4.5/systemvm64template-4.5-kvm.qcow2.bz2' where id=1;"
 mysql -u cloud -pcloud cloud --exec "UPDATE cloud.vm_template SET url='http://dl.openvm.eu/cloudstack/macchinina/x86_64/macchinina-kvm.qcow2.bz2', guest_os_id=140, name='tiny linux kvm', display_text='tiny linux kvm', hvm=1 where id=2;"
 
 # Run mgt
