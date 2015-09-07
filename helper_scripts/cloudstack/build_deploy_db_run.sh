6da566d5b384ef4e214aefe87138555c0a653926
Boris Schrijver <boris@pcextreme.nl>
2015-09-08 00:20:05 +0200

    Added build_deploy_db_run.sh script.

---
+++ helper_scripts/cloudstack/build_deploy_db_run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+host_ip=`ip addr | grep 'inet 192' | cut -d: -f2 | awk '{ print $2 }' | awk -F\/24 '{ print $1 }'`
+
+# We work from here
+cd /data/git/$HOSTNAME/cloudstack
+
+# Compile ACS
+mvn clean install -P developer,systemvm -DskipTests -T 2C
+
+# Deploy DB
+mvn -P developer -pl developer -Ddeploydb
+
+# Configure the hostname properly - it doesn't exist if the deployeDB doesn't include devcloud
+mysql -u cloud -pcloud cloud --exec "INSERT INTO cloud.configuration (instance, name, value) VALUE('DEFAULT', 'host', '$host_ip') ON DUPLICATE KEY UPDATE value = '$host_ip';"
+
+# Adding the right SystemVMs, for KVM
+# Adding the tiny linux VM templates for KVM
+mysql -u cloud -pcloud cloud --exec "UPDATE cloud.vm_template SET url='http://cloudstack.apt-get.eu/systemvm/4.5/systemvm64template-4.5-kvm.qcow2.bz2' where id=1;"
+mysql -u cloud -pcloud cloud --exec "UPDATE cloud.vm_template SET url='http://dl.openvm.eu/cloudstack/macchinina/x86_64/macchinina-kvm.qcow2.bz2', guest_os_id=140, name='tiny linux kvm', display_text='tiny linux kvm', hvm=1 where id=2;"
+
+# Run mgt
+mvn -pl :cloud-client-ui jetty:run
+
